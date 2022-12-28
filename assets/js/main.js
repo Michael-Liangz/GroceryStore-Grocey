6ea990d370b249bcf441d4368c92b54926049a86
Michael-Liangz <Michaelliangz.ml@gmail.com>
2022-12-28 18:38:12 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,10 @@ let shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#cart-btn").onclick = () => {
   shoppingCart.classList.toggle("active");
 };
+
+// TODO ======================= HEADER LOGIN FORM =======================
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
