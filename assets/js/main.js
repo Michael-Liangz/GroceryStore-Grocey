2be17dd4c2bbbafa757c99a25fd0adaac115e9fa
Michael-Liangz <Michaelliangz.ml@gmail.com>
2022-12-28 19:55:24 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ let searchForm = document.querySelector(".search-form");
 
 document.querySelector("#search-btn").onclick = () => {
   searchForm.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
 };
 
 // TODO ======================= HEADER SHOPPING CART =======================
@@ -10,6 +13,9 @@ let shoppingCart = document.querySelector(".shopping-cart");
 
 document.querySelector("#cart-btn").onclick = () => {
   shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
 };
 
 // TODO ======================= HEADER LOGIN FORM =======================
@@ -17,4 +23,24 @@ let loginForm = document.querySelector(".login-form");
 
 document.querySelector("#login-btn").onclick = () => {
   loginForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+// TODO ======================= HEADER NAVBAR =======================
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
 };
