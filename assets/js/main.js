fc7aeca574883e744c264303764bedcb71ffa65f
Michael-Liangz <Michaelliangz.ml@gmail.com>
2022-12-29 20:07:47 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,25 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
+
+// TODO ======================= SWIPER JS =======================
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
